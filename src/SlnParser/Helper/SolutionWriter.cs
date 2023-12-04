182201f99b7b9f89c57025c5e3d20140323b0a72
Tim Abell <tim@timwise.co.uk>
2023-12-04 21:02:48 +0000

    Write out ConfigurationPlatforms

--- src/SlnParser/Helper/SolutionWriter.cs
+++ src/SlnParser/Helper/SolutionWriter.cs
@@ -26,7 +26,8 @@ namespace SlnParser.Helper
             sb.AppendLine($"MinimumVisualStudioVersion = {solution.VisualStudioVersion.MinimumVersion}");
             foreach (var project in solution.AllProjects)
             {
-                sb.AppendLine($"Project(\"{{{project.TypeGuid.ToString().ToUpper()}}}\") = \"{project.Name}\", \"{project.Path}\", \"{{{project.Id.ToString().ToUpper()}}}\"");
+                sb.AppendLine(
+                    $"Project(\"{{{project.TypeGuid.ToString().ToUpper()}}}\") = \"{project.Name}\", \"{project.Path}\", \"{{{project.Id.ToString().ToUpper()}}}\"");
                 if (project is SolutionFolder solutionFolder && solutionFolder.Files.Any())
                 {
                     sb.AppendLine("\tProjectSection(SolutionItems) = preProject");
@@ -34,57 +35,22 @@ namespace SlnParser.Helper
                     {
                         sb.AppendLine($"\t\t{file.Name} = {file.Name}");
                     }
+
                     sb.AppendLine("\tEndProjectSection");
                 }
+
                 sb.AppendLine("EndProject");
             }
+
             sb.AppendLine("Global");
+            sb.AppendLine("\tGlobalSection(SolutionConfigurationPlatforms) = preSolution");
+            foreach (var platform in solution.ConfigurationPlatforms)
+            {
+                sb.AppendLine($"\t\t{platform.Name} = {platform.Name}");
+            }
+
+            sb.AppendLine("\tEndGlobalSection");
             sb.AppendLine("EndGlobal");
-            /*
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Any CPU = Debug|Any CPU
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|Any CPU = Release|Any CPU
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Debug|x64.ActiveCfg = Debug|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Debug|x64.Build.0 = Debug|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Debug|x86.ActiveCfg = Debug|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Debug|x86.Build.0 = Debug|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Release|Any CPU.Build.0 = Release|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Release|x64.ActiveCfg = Release|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Release|x64.Build.0 = Release|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Release|x86.ActiveCfg = Release|Any CPU
-		{EDC2B9FC-02D0-4541-8484-CAB27B00252D}.Release|x86.Build.0 = Release|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Debug|x64.ActiveCfg = Debug|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Debug|x64.Build.0 = Debug|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Debug|x86.ActiveCfg = Debug|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Debug|x86.Build.0 = Debug|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Release|Any CPU.Build.0 = Release|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Release|x64.ActiveCfg = Release|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Release|x64.Build.0 = Release|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Release|x86.ActiveCfg = Release|Any CPU
-		{BB52A27D-766E-4ECD-B888-BD86405134C1}.Release|x86.Build.0 = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {7F92F20E-4C3D-4316-BF60-105559EFEAFF}
-	EndGlobalSection
-EndGlobal
-");
-*/
             return sb.ToString();
         }
     }
