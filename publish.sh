78cd9b6080669107e14e2fecae628311735f05aa
Tim Abell <tim@timwise.co.uk>
2023-12-22 01:06:03 +0000

    Make publish.sh version agnostic

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -v
 set -e
+git clean -xfd
 dotnet pack
-dotnet nuget push SlnEditor/bin/Debug/SlnEditor.1.0.0.nupkg --api-key="$apikey" --source https://api.nuget.org/v3/index.json
+ls -al SlnEditor/bin/Release/SlnEditor.*.nupkg
+dotnet nuget push SlnEditor/bin/Release/SlnEditor.*.nupkg --api-key="$apikey" --source https://api.nuget.org/v3/index.json
