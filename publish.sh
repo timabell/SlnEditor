c061a083beaf7f4ffc00144857251817a9acc613
Tim Abell <tim@timwise.co.uk>
2024-11-19 22:59:33 +0000

    Remove publish script
    Being done with github pipeline now

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -v
-set -e
-git clean -xfd
-dotnet pack
-ls -al SlnEditor/bin/Release/SlnEditor.*.nupkg
-dotnet nuget push SlnEditor/bin/Release/SlnEditor.*.nupkg --api-key="$apikey" --source https://api.nuget.org/v3/index.json
